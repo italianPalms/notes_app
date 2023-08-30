60bab90bd7dcce1ca2ee8b3fbb29048f4109995d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-08-30 22:14:39 +0200

    adjust syntax errors

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,13 @@
+"""
+This module contains a flask application for a simple web service
+"""
+
 from flask import Flask
 
 def create_app():
+    """
+    Create and configure the Flask app.
+    """
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'wkslhjykksks'
-    return app
+    return app 
